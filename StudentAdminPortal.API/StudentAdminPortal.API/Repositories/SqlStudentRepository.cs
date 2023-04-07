6eebfb32611ba22ca0e08e485ab750b9fd57cbbc
Amy <amy.mai97@yahoo.com>
2023-04-06 21:04:44 -0500

    Added GetAllStudent with AutoMapper

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Repositories/SqlStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using StudentAdminPortal.API.DataModels;
+using Microsoft.EntityFrameworkCore;
+namespace StudentAdminPortal.API.Repositories
+{
+    public class SqlStudentRepository : IStudentRepository
+    {
+        private readonly StudentAdminContext studentAdminCOntext;
+        public SqlStudentRepository(StudentAdminContext studentAdminContext)
+        {
+            this.studentAdminCOntext = studentAdminContext;
+        }
+        public async Task<List<Student>> GetStudents()
+        {
+            return await studentAdminCOntext.Student.Include(nameof(Gender)).Include(nameof(Address)).ToListAsync();
+        }
+    }
+}
